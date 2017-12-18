4203c0dc5207dff0b1b643cc5dd03611ae0fbc45
Krzysztof Ruszczynski <ruszczak@o2.pl>
2017-12-18 16:37:09 +0100

    update DataDogAuditExtension so data work by every execution

--- src/DataDog/AuditBundle/DependencyInjection/DataDogAuditExtension.php
+++ src/DataDog/AuditBundle/DependencyInjection/DataDogAuditExtension.php
@@ -20,11 +20,11 @@ class DataDogAuditExtension extends Extension
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
 
-        $auditSubscriber = $container->getDefinition('datadog.event_subscriber.audit');
-
         if (isset($config['audited_entities']) && !empty($config['audited_entities'])) {
+            $auditSubscriber = $container->getDefinition('datadog.event_subscriber.audit');
             $auditSubscriber->addMethodCall('addAuditedEntities', array($config['audited_entities']));
         } else if (isset($config['unaudited_entities'])) {
+            $auditSubscriber = $container->getDefinition('datadog.event_subscriber.audit');
             $auditSubscriber->addMethodCall('addUnauditedEntities', array($config['unaudited_entities']));
         }
     }
